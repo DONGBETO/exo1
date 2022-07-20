b770d77ab0d84265b5a3a5144cb2e1d4cc245428
Emmanuel <christianemmanueldongbeto@gmail.com>
2022-07-21 00:05:20 +0100

    Ajout de code

---
+++ lib/Screen.dart/formulaire.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:exo_flutter/widget.dart/input.dart';
+
+class Formulaire extends StatelessWidget {
+  const Formulaire({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        'Page de connexion',
+        style: TextStyle(backgroundColor: Colors.blue),
+      ),
+      ),
+      body: Input(),
+    );
+  }
+}
